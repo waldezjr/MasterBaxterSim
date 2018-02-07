55888655b33547b018646e22ed131b7bbccde4a3
Waldez Junior <waldezjr14@gmail.com>
2018-02-07 19:35:30 +0100

    publish joint configuration to baxter

--- src/jointPositionPublisherExample.py
+++ src/jointPositionPublisherExample.py
@@ -5,20 +5,47 @@ import numpy
 #from baxter_pykdl import baxter_kinematics
 import baxter_interface
 
-def printCurJointPos():
-	print("testing")
-	rospy.loginfo(" ROS")
+from baxter_core_msgs.msg import (
+    JointCommand,
+)
+
+def printCurJointPos(limb):
+	#print("testing")
+	#rospy.loginfo(" ROS")
+	print(limb.joint_angles())
+	#print(limb.joint_names())
+
 
 def main():
 
 	rospy.init_node('jointPositionPublisherExample', anonymous=True)
-	rate = rospy.Rate(10)
+	pub_joint_cmd = rospy.Publisher('/robot/limb/left/joint_command', JointCommand, queue_size=10)
+
+	limb = baxter_interface.Limb("left")
+	limb.set_joint_position_speed(0.5)
+	limb.set_command_timeout(120.0)
+
+	cmdMsg = JointCommand()
+	cmdMsg.mode = JointCommand.POSITION_MODE
+
+#		q untucked, considered to be the initial q: 'left_w0': 0.669996718047412, 'left_w1': 1.030008750129423, 'left_w2': -0.4999996908801263, 'left_e0': -1.1899720096239292, 'left_e1': 1.940029476507764, 'left_s0': -0.08000000085054282, 'left_s1': -0.9999845808672081
+
+	q = limb.joint_angles()	#joint configuration
+	q['left_w2'] = 3
+	cmdMsg.command = q.values()
+	cmdMsg.names = q.keys()
+
+	print(cmdMsg)
+	pub_joint_cmd.publish(cmdMsg)
+
+	#positions = dict(zip(self.limb.joint_names(), theta))
+
+	rate = rospy.Rate(2)
 
 	while not rospy.is_shutdown():
-		#hello_str = "hello world %s" % rospy.get_time()
-		#rospy.loginfo(hello_str)
-		#pub.publish(hello_str)
-		printCurJointPos()
+		printCurJointPos(limb)
+		pub_joint_cmd.publish(cmdMsg)
+
 		rate.sleep()
 
 if __name__ == '__main__':
