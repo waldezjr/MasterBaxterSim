de5974c6206ba15a38e81e4a3424fb9a984b21e9
Waldez Junior <waldezjr14@gmail.com>
2018-02-27 15:29:46 +0100

    jacobian from arm parameterized xacro

--- nodes/pykdltests.py
+++ nodes/pykdltests.py
@@ -2,8 +2,26 @@
 from urdf_parser_py.urdf import URDF
 from pykdl_utils.kdl_kinematics import KDLKinematics
 import numpy as np
+import subprocess
+import sys
 
-robot = URDF.from_parameter_server()
+l1 = 0.6 #upper arm length
+l2 = 0.3 #forearm length
+l3 = 0.15 #hand (fist) length
+command_str = "rosrun xacro xacro /home/waldezjr/catkin_ws/src/master_baxter_sim/robots/human_right_arm.xacro l1:={0} l2:={1} l3:={2}".format(l1,l2,l3)
+print 'command_str', command_str
+
+#run xacro and get urdf as string
+try:
+    robot_description = subprocess.check_output(
+        command_str, shell=True, stderr=subprocess.STDOUT)
+except subprocess.CalledProcessError as process_error:
+    print process_error.output
+    sys.exit(1)
+
+# print robot_description
+
+robot = URDF.from_xml_string(robot_description)
 kdl_kin = KDLKinematics(robot, "base_link", "EEF")
 q = kdl_kin.random_joint_angles()
 pose = kdl_kin.forward(q) # forward kinematics (returns homogeneous 4x4 numpy.mat)
@@ -23,6 +41,6 @@ Jpos = J[:3,:]
 print 'Jpos',Jpos
 # J is a numpy matrix
 
-manipulabilty = np.sqrt(np.linalg.det(Jpos*Jpos.transpose()))
+manipulability = np.sqrt(np.linalg.det(Jpos*Jpos.transpose()))
 
-print 'manipulabilty', manipulabilty
+print 'manipulability', manipulability
