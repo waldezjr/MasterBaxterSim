e7d225c381b8a66bf1f84f13ff34dfed0a8b1ddd
Waldez Junior <waldezjr14@gmail.com>
2018-02-27 06:36:11 +0100

    calculating human arm manipulability

---
+++ nodes/pykdltests.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from urdf_parser_py.urdf import URDF
+from pykdl_utils.kdl_kinematics import KDLKinematics
+import numpy as np
+
+robot = URDF.from_parameter_server()
+kdl_kin = KDLKinematics(robot, "base_link", "EEF")
+q = kdl_kin.random_joint_angles()
+pose = kdl_kin.forward(q) # forward kinematics (returns homogeneous 4x4 numpy.mat)
+q_ik = kdl_kin.inverse(pose, q+0.3) # inverse kinematics
+if q_ik is not None:
+    pose_sol = kdl_kin.forward(q_ik) # should equal pose
+J = kdl_kin.jacobian(q)
+print 'q:', q
+print 'q_ik:', q_ik
+print 'pose:', pose
+if q_ik is not None:
+    print 'pose_sol:', pose_sol
+print 'J:', J
+
+Jpos = J[:3,:]
+
+print 'Jpos',Jpos
+# J is a numpy matrix
+
+manipulabilty = np.sqrt(np.linalg.det(Jpos*Jpos.transpose()))
+
+print 'manipulabilty', manipulabilty
